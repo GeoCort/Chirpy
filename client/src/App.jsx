cbe7861d2511fbfef9c80e9996585d90a0e3c4b5
george cortes <cortes.george93@icloud.com>
2024-11-19 02:46:40 -0800

    changed sidebar

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -9,7 +9,7 @@ function App() {
   const [count, setCount] = useState(0)
   const [darkMode, setDarkMode] = useState(false)
   return (
-    <main className=' min-h-screen grid  grid-cols-[100px_1fr] lg:grid-cols-[100px_1fr_350px] xl:mx-[10%]'>
+    <main className=' min-h-screen grid  grid-cols-[100px_1fr] lg:grid-cols-[200px_1fr_350px] xl:mx-[10%]'>
       <Sidebar toggleDark={darkMode}/>
      <Timeline toggleDark={darkMode}/>
      <Features toggleDark={darkMode}/>
--- client/src/Sidebar.jsx
+++ client/src/Sidebar.jsx
@@ -10,12 +10,18 @@ export default function Sidebar(props){
     const [darkMode, setDarkMode] = useState("border-zinc-50 bg-zinc-800 text-zinc-50")
     
     return(
-    <div className={`${props.darkMode?darkMode:lightMode} col-span-1 border-r-2 flex flex-col text-5xl gap-5 items-center p-5 px-2`}>
+    <div className={`${props.darkMode?darkMode:lightMode} col-span-1 border-r-2 flex flex-col text-5xl lg:text-3xl gap-5 items-center p-5 px-2`}>
         <img src={chirpy} className="w-16 h-16 rounded-full border-2"/>
-        <Link><FaHome/></Link>
-        <Link><HiUsers/></Link>
-        <Link> <CgProfile /></Link>
-        <Link><IoSettingsSharp /></Link>
+        <div className="flex gap-4"><Link><FaHome/></Link><span className="hidden lg:inline text-2xl ">Home</span></div>
+        <div className="flex gap-4">
+        <Link><HiUsers/></Link><span className="hidden lg:inline text-2xl">Users</span>
+        </div>
+        <div className="flex gap-4">
+        <Link> <CgProfile /></Link><span className="hidden lg:inline text-2xl" >Profile</span>
+        </div>
+        <div className="flex gap-4">
+        <Link><IoSettingsSharp /></Link><span className="hidden lg:inline text-2xl">Settings</span>
+        </div>
     </div>
 )
 }
