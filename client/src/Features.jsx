19c4805ed7d13e9b5ae9523c8e542c9f5d4b64c9
george cortes <cortes.george93@icloud.com>
2024-11-20 13:43:53 -0800

    stylying buttons and padding for features,sideline, and timeline.

--- client/src/Features.jsx
+++ client/src/Features.jsx
@@ -2,27 +2,31 @@ import { useState } from "react"
 export default function Features(props){
     const [lightMode, setLightMode ] = useState("border-black bg-zinc-50 text-zinc-800")
     const [darkMode, setDarkMode] = useState("border-zinc-50 bg-zinc-800 text-zinc-50")
-    
+    const articleClass = "p-4 flex flex-col gap-2"
+    const buttonClass ="ml-auto block py-2 px-4 bg-blue-600 text-white rounded-md hover:bg-blue-800"
+    const headers = "text-2xl text-center py-2 font-semibold"
+    const linksClass = "text-blue-500 font-semibold"
     return(
         <div className={`mx-3 lg:flex flex-col gap-5 hidden p-5 ${props.darkMode?darkMode:lightMode}`}>
-        <input type="text" className="rounded-lg p-2 " placeholder="Search"/>
-        <article className="border-2 rounded-lg p-2 ">
-            <button className="ml-auto block py-1 px-2 bg-slate-500 rounded-lg">Subscribe</button>
+        <input type="text" className="rounded-lg p-2 bg-zinc-50 border-2 " placeholder="Search"/>
+        <article className="border-2 rounded-lg p-2 flex items-center">
+            <p className="text-xl">Buy Premium</p>
+            <button className={buttonClass}>Subscribe</button>
         </article>
         <section className="border-2 rounded-lg ">
-            <article>
-                <h2 className="text-2xl text-center">Welcome New Users</h2>
-                New Users
-                <p>Michael</p>
-                <p>Lisa</p>
+            <article className={articleClass}>
+                <h2 className={headers}>Newest Members</h2>
+                <span className={linksClass}>Lilith</span>
+                <span className={linksClass}>Lilith</span>
+                <span className={linksClass}>Lilith</span>
             </article>
         </section>
         <section className="border-2 rounded-lg ">
-            <article>
-                <h2 className="text-2xl text-center">New Posts</h2>
-                New Users
-                <p>Michael</p>
-                <p>Lisa</p>
+            <article className={articleClass}>
+                <h2 className={headers}>Recent Activity</h2>
+                <span className={linksClass}>Lilith</span>
+                <span className={linksClass}>Lilith</span>
+                <span className={linksClass}>Lilith</span>
             </article>
         </section>
         </div>
--- client/src/Sidebar.jsx
+++ client/src/Sidebar.jsx
@@ -8,20 +8,14 @@ import { useState } from "react";
 export default function Sidebar(props){
     const [lightMode, setLightMode ] = useState("border-black bg-zinc-50 text-zinc-800")
     const [darkMode, setDarkMode] = useState("border-zinc-50 bg-zinc-800 text-zinc-50")
-    
+    const linkButton = "py-5 px-4  flex gap-2 hover:text-white hover:bg-zinc-600"
     return(
-    <div className={`${props.darkMode?darkMode:lightMode} col-span-1 border-r-2 flex flex-col text-5xl lg:text-3xl gap-5 items-center p-5 px-2`}>
+    <div className={`${props.darkMode?darkMode:lightMode} col-span-1 border-r-2 flex flex-col text-5xl lg:text-3xl gap-2 items-center py-10`}>
         <img src={chirpy} className="w-16 h-16 rounded-full border-2"/>
-        <div className="flex gap-4"><Link><FaHome/></Link><span className="hidden lg:inline text-2xl ">Home</span></div>
-        <div className="flex gap-4">
-        <Link><HiUsers/></Link><span className="hidden lg:inline text-2xl">Users</span>
-        </div>
-        <div className="flex gap-4">
-        <Link> <CgProfile /></Link><span className="hidden lg:inline text-2xl" >Profile</span>
-        </div>
-        <div className="flex gap-4">
-        <Link><IoSettingsSharp /></Link><span className="hidden lg:inline text-2xl">Settings</span>
-        </div>
+        <Link className={linkButton}><FaHome/><span className="hidden lg:inline text-2xl ">Home</span></Link>
+        <Link className={linkButton}><HiUsers/> <span className="hidden lg:inline text-2xl">Users</span></Link>
+        <Link className={linkButton}> <CgProfile /><span className="hidden lg:inline text-2xl" >Profile</span> </Link>
+        <Link className={linkButton}><IoSettingsSharp /> <span className="hidden lg:inline text-2xl">Settings</span></Link>
     </div>
 )
 }
